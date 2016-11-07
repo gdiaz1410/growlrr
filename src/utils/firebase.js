99e77599a4e7c28da6df0716188eca5bfa6076a1
Connor Garber <czaneg@gmail.com>
2016-11-07 10:35:17 -0600

    firebase boilerplate

---
+++ src/utils/firebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+import firebase from 'firebase'
+
+var config = {
+  apiKey: "AIzaSyBtY1rvAE4yNKZpC-SCS6ifn7_0UBDMBaA",
+  authDomain: "growlrr-76626.firebaseapp.com",
+  databaseURL: "https://growlrr-76626.firebaseio.com",
+  storageBucket: "growlrr-76626.appspot.com",
+  messagingSenderId: "717202556880"
+};
+export default firebase.initializeApp(config);
